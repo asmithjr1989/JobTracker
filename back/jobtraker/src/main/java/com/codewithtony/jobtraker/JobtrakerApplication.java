c1f5212ba756358d2f1e18d4f6599ec3dd208f43
Anthony Smith <asmithjr1989@gmail.com>
2020-12-07 10:22:28 -0600

    added spring starter download

---
+++ back/jobtraker/src/main/java/com/codewithtony/jobtraker/JobtrakerApplication.java
@@ -0,0 +1,13 @@
+package com.codewithtony.jobtraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtrakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobtrakerApplication.class, args);
+	}
+
+}
---
+++ back/jobtraker/src/test/java/com/codewithtony/jobtraker/JobtrakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithtony.jobtraker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

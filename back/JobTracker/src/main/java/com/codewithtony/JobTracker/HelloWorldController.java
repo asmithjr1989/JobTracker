7629e434043a90fb070b0951c6cdf2bc2ade713f
Anthony Smith <asmithjr1989@gmail.com>
2020-12-07 11:19:11 -0600

    added hello world controller for practice

---
+++ back/JobTracker/src/main/java/com/codewithtony/JobTracker/HelloWorldController.java
@@ -0,0 +1,30 @@
+package com.codewithtony.JobTracker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+
+    //GET
+    //URI /hello-world
+    //method to return hello world
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+
+    }
+
+}
